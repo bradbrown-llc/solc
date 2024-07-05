a4283b2c66e389c435cac6e350e17d7cdd13f492
Brad Brown <bradbrown@bradbrown.llc>
2024-07-05 18:35:45 +0000

    make more things optional

--- schemas/solcOut/contracts/bytecode.ts
+++ schemas/solcOut/contracts/bytecode.ts
@@ -1,3 +1,3 @@
 import z from 'https://deno.land/x/zod@v3.22.4/index.ts';
 
-export const bytecode = z.object({ object: z.string() }).optional()
+export const bytecode = z.object({ object: z.string().optional() }).optional()
--- schemas/solcOut/contracts/evm.ts
+++ schemas/solcOut/contracts/evm.ts
@@ -1,4 +1,4 @@
 import z from 'https://deno.land/x/zod@v3.22.4/index.ts';
 import { bytecode } from './bytecode.ts';
 
-export const evm = z.object({ bytecode })
+export const evm = z.object({ bytecode }).optional()

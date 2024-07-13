b4bd39e204e86a98ef8a7863d635ade0c854c3f3
Brad Brown <bradbrown@bradbrown.llc>
2024-07-13 17:12:44 +0000

    goofy, but removes double slash nonsense

--- tests/weth/test.ts
+++ tests/weth/test.ts
@@ -3,7 +3,8 @@ import { Solc } from "../../Solc.ts";
 import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 
 const solcDir = fromFileUrl(import.meta.resolve('../../.cache'))
-const projDir = fromFileUrl(import.meta.resolve('./.'))
+const projDir = fromFileUrl(import.meta.resolve('../weth'))
+console.log({ solcDir, projDir })
 const solcJsonInputPath = `${projDir}/settings.json`
 const results = await Solc.compile(solcJsonInputPath, solcDir)
 const bytecode = results?.contracts?.['WETH9.sol']?.['WETH9']?.evm?.bytecode?.object

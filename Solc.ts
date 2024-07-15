0aab14004377f99f7891f7f59e037a7b4dae2535
Brad Brown <bradbrown@bradbrown.llc>
2024-07-15 11:29:47 +0000

    isolate some functions and add simple compilation (direct code string)

--- Solc.ts
+++ Solc.ts
@@ -1,8 +1,11 @@
 import * as SV from 'https://deno.land/std@0.224.0/semver/mod.ts'
 import * as Path from 'https://deno.land/std@0.224.0/path/mod.ts'
 import * as schemas from './schemas/mod.ts'
+import { acquireSolcRelease, solidityToSemVerRange, updateSolcVersionsList } from './lib/mod.ts'
 
 export class Solc {
+
+
 
     static async compile(solcJsonInputPath:string, solcDir:string) {
 
@@ -29,31 +32,14 @@ export class Solc {
         // get SemVer Ranges from code (and code from source urls)
         const sourceContents:string[] = []
         for (const path of sourcePaths) sourceContents.push(await Deno.readTextFile(path))
-        const ranges:SV.Range[] = []
-        for (const code of sourceContents) {
-            const versionPragmaText = code.match(/pragma solidity (.+?);/)?.[1]
-            if (!versionPragmaText) throw new Error('no solidity version detected')
-            const range = SV.parseRange(versionPragmaText)
-            ranges.push(range)
-        }
+        const ranges = sourceContents.map(solidityToSemVerRange)
     
         // make sure we have a relatively up-to-date list of solc versions
-        const solcVersionsListPath = `${solcDir}/versions.json`
-        const solcVersionsListUrl = 'https://binaries.soliditylang.org/linux-amd64/list.json'
-        const solcVersionsListExists = await Deno.stat(solcVersionsListPath)
-            .catch<false>(() => false)
-        const solcVersionsListMTime = solcVersionsListExists
-            ? solcVersionsListExists.mtime!.getTime()!
-            : -Infinity
-        const solcVersionsListOutdated = Date.now() - solcVersionsListMTime > 86400000
-        if (!solcVersionsListExists || solcVersionsListOutdated) {
-            const response = await fetch(solcVersionsListUrl)
-            await Deno.writeTextFile(solcVersionsListPath, await response.text())
-        }
+        await updateSolcVersionsList(solcDir)
     
         // turn list file into object, get versions, find best version for our ranges
         // get release from best version
-        const solcVersionsListText = await Deno.readTextFile(solcVersionsListPath)
+        const solcVersionsListText = await Deno.readTextFile(`${solcDir}/versions.json`)
         const solcVersionsList = await schemas.solcVersionsList.parseAsync(JSON.parse(solcVersionsListText))
         let versions = Object.keys(solcVersionsList.releases).map(SV.parse)
         for (const range of ranges) versions = versions.filter(v => SV.satisfies(v, range))
@@ -62,11 +48,7 @@ export class Solc {
         const release = solcVersionsList.releases[`${v.major}.${v.minor}.${v.patch}`]
     
         // acquire the release if needed
-        if (!await Deno.stat(`${solcDir}/${release}`).catch(()=>0)) {
-            const response = await fetch(`https://binaries.soliditylang.org/linux-amd64/${release}`)
-            const blob = await response.blob()
-            await Deno.writeFile(`${solcDir}/${release}`, blob.stream(), { mode: 0o755 })
-        }
+        await acquireSolcRelease(solcDir, release)
     
         // compile
         const args = ['--standard-json', '--allow-paths', allowPaths.join(',')]
@@ -80,5 +62,54 @@ export class Solc {
         return await schemas.solcCompilationOutput.parseAsync(JSON.parse(stdout))
     
     }
+
+
+
+    static async compileSimple(code:string, solcDir:string) {
+
+        // get SemVer range from code
+        const range = solidityToSemVerRange(code)
+
+        // turn list file into object, get versions, find best version for our range
+        // get release
+        const solcVersionsListText = await Deno.readTextFile(`${solcDir}/versions.json`)
+        const solcVersionsList = await schemas.solcVersionsList.parseAsync(JSON.parse(solcVersionsListText))
+        const versions = Object.keys(solcVersionsList.releases).map(SV.parse).filter(v => SV.satisfies(v, range))
+        if (!versions.length) throw new Error('no version satisfies ranges')
+        const v = versions.reduce((p, c) => SV.greaterThan(c, p) ? c : p)
+        const release = solcVersionsList.releases[`${v.major}.${v.minor}.${v.patch}`]
+
+        // acquire the release if needed
+        await acquireSolcRelease(solcDir, release)
+
+        // compile
+        const args = ['--standard-json']
+        const options = { args, stdin: 'piped', stdout: 'piped', stderr: 'piped' } as const
+        const proc = new Deno.Command(`${solcDir}/${release}`, options).spawn()
+        const writer = proc.stdin.getWriter()
+        const evmVersion
+            = v.minor < 5 || (v.minor == 5 && v.patch < 5) ? 'byzantium'
+            : v.minor == 5 && v.patch < 14 ? 'petersburg'
+            : v.minor < 8 || (v.minor == 8 && v.patch < 5) ? 'istanbul'
+            : v.minor == 8 && v.patch < 7 ? 'berlin'
+            : v.minor == 8 && v.patch < 18 ? 'london'
+            : 'paris'
+        await writer.write(new TextEncoder().encode(JSON.stringify({
+            language: 'Solidity',
+            sources: { '': { content: code } },
+            settings: {
+                outputSelection: { '*': { '*': ['evm.bytecode.object'] } },
+                evmVersion
+            }
+        })))
+        await writer.close()
+        const cmdOut = await proc.output()
+        const stdout = new TextDecoder().decode(cmdOut.stdout)
+        console.log(stdout)
+        return await schemas.solcCompilationOutput.parseAsync(JSON.parse(stdout))
+        
+    }
+
+
 
 }

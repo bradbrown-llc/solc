815ecf88a209e17b9b54464eeb75861c7c3b70da
Brad Brown <bradbrown@bradbrown.llc>
2024-07-15 11:30:05 +0000

    DZHV resolver simple compilation test

---
+++ tests/compileSimple/test.ts
@@ -0,0 +1,16 @@
+import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
+import { fromFileUrl } from "https://deno.land/std@0.224.0/path/from_file_url.ts";
+import { Solc } from "../../Solc.ts";
+
+const solcDir = fromFileUrl(import.meta.resolve('../../.cache'))
+const code = Deno.readTextFileSync(fromFileUrl(import.meta.resolve('./Resolver.sol')))
+    .replace(/\?I\?+/, ''.padEnd(40, '0'))
+    .replace(/\?D\?+/, ''.padEnd(40, '1'))
+    .replace(/\?W\?+/, ''.padEnd(40, '2'))
+
+const results = await Solc.compileSimple(code, solcDir)
+const bytecode = results?.contracts?.['']?.['Resolver']?.evm?.bytecode?.object
+Deno.test('bytecode exists', () => {
+    console.log(bytecode)
+    assert(bytecode)
+})
